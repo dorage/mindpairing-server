d76d66cb5e0562f2209e0beb04db857c46f39412
Park donghyeon <emma415g@gmail.com>
2023-07-24 22:36:30 +0900

    add user interest

--- mbti/serializers.py
+++ mbti/serializers.py
@@ -10,7 +10,12 @@ class MBTIQuestionSerializer(serializers.ModelSerializer):
 
 class MBTIClassSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(use_url=True)
-    
+
+    def get_matches(self, mbti_class: MBTIClass):
+        return [mbti_class.match[:4].upper(), mbti_class.match[4:].upper()]
+
+    matches = serializers.SerializerMethodField(source=get_matches)
+
     class Meta:
         model = MBTIClass
-        fields = ('mbti', 'title', 'summary', 'description', 'love', 'advice', 'match', 'image')
+        fields = ('mbti', 'title', 'summary', 'description', 'love', 'advice', 'matches', 'image')
